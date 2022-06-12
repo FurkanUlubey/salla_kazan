e1732a2ab9c46742dedc461686765bcea221f954
Prens_J <99231918+PrnsJ@users.noreply.github.com>
2022-06-13 01:07:45 +0300

    Create salla_kazan.py

---
+++ salla_kazan.py
@@ -0,0 +1,111 @@
+from random import *
+from colorama import init
+from termcolor import colored as cl
+    #  input("Devam etmek için entera basınız.")
+print( cl("SALLA KAZANA HOŞGELDİNİZ!!!","yellow"))
+
+input("Sallamak için Enter'a basın"),
+input("Son bir kez daha sallayın... Enter'a basın")
+
+paket = randint(1,50)
+
+if paket==1:
+    print("AYLIK TV+ PAKETİ KAZANDINIZ!!")
+elif paket==2:
+    print("AYLIK Fizy PREMİUM PAKETİ KAZANDINIZ!!")
+elif paket==3:
+    print("2 AYLIK Fizy PREMİUM PAKETİ KAZANDINIZ!!")
+elif paket==4:
+    print("3 AYLIK Lifebox ÜYELİK PAKETİ KAZANDINIZ!!")
+elif paket==5:
+    print("AYLIK BiP ÜZERİNDEN HER YÖNE 1.000 DAKİKA KAZANDINIZ!!")
+elif paket==6:
+    print("AYLIK BiP ÜZERİNDEN HER YÖNE 500 DAKİKA KAZANDINIZ!!")
+elif paket==7:
+    print("HAFTALIK BiP ÜZERİNDEN HER YÖNE 1.000 DAKİKA KAZANDINIZ!!")
+elif paket==8:
+    print("HAFTALIK BiP ÜZERİNDEN HER YÖNE 500 DAKİKA KAZANDINIZ!!")
+elif paket==9:
+    print("AYLIK DERGİLİK PAKETİ KAZANDINIZ!!")
+elif paket==10:
+    print("HAFTALIK GollerCepte SÜPER PAKETİ KAZANDINIZ!!")
+elif paket==11:
+    print("HAFTALIK Hürriyet PAKETİ KAZANDINIZ!!")
+elif paket==12:
+    print("APP STORE'DA GEÇERLİ 10 TL HEDİYE KAZANDINIZ!!")
+elif paket==13:
+    print("GOOGLE PLAY'DE GEÇERLİ 10 TL HEDİYE KAZANDINIZ!!")
+elif paket==14:
+    print("APP STORE'DA GEÇERLİ 5 TL HEDİYE KAZANDINIZ!!")
+elif paket==15:
+    print("GOOGLE PLAY'DE GEÇERLİ 5 TL HEDİYE KAZANDINIZ!!")
+elif paket==16:
+    print("League of Legends OYUNUNDA GEÇERLİ 10 TL HEDİYE KAZANDINIZ!!")
+elif paket==17:
+    print("Zula OYUNUNDA GEÇERLİ 10 TL HEDİYE KAZANDINIZ!!")
+elif paket==18:
+    print("Paycell HEDİYE DÜNYAM'DA GEÇERLİ 1000 PUAN")
+elif paket==20:
+    print("AYLIK 25 GB İNTERNET KAZANDINIZ!!")
+elif paket==21:
+    print("AYLIK 10 GB İNTERNET KAZANDINIZ!!")
+elif paket==22:
+    print("AYLIK 3 GB İNTERNET KAZANDINIZ!!")
+elif paket==23:
+    print("3 AY TEKRARLI AYLIK 3 GB KAZANDINIZ!!")
+elif paket==24:
+    print("3 AY TEKRARLI AYLIK 1 GB KAZANDINIZ!!")
+elif paket==25:
+    print("HAFTALIK 10 GB İNTERNET KAZANDINIZ!!")
+elif paket==26:
+    print("HAFTALIK 5 GB İNTERNET KAZANDINIZ!!")
+elif paket==27:
+    print("HAFTALIK 4 GB İNTERNET KAZANDINIZ!!")
+elif paket==28:
+    print("HAFTALIK 3 GB İNTERNET KAZANDINIZ!!")
+elif paket==29:
+    print("HAFTALIK 2 GB İNTERNET KAZANDINIZ!!")
+elif paket==30:
+    print("HAFTALIK 1 GB İNTERNET KAZANDINIZ!!")
+elif paket==31:
+    print("2 HAFTALIK 1 GB İNTERNET KAZANDINIZ!!")
+elif paket==32:
+    print("HAFTALIK 500 MB İNTERNET KAZANDINIZ!!")
+elif paket==33:
+    print("GÜNLÜK 1 GB İNTERNET KAZANDINIZ!!")
+elif paket==34:
+    print("GÜNLÜK 2 GB İNTERNET KAZANDINIZ!!")
+elif paket==35:
+    print("2 GÜNLÜK 1 GB İNTERNET KAZANDINIZ!!")
+elif paket==36:
+    print("GÜNLÜK 500 MB İNTERNET KAZANDINIZ!!")
+elif paket==37:
+    print("12 SAATLİK 2 GB İNTERNET KAZANDINIZ!!")
+elif paket==38:
+    print("12 SAATLİK 1 GB İNTERNET KAZANDINIZ!!")
+elif paket==39:
+    print("6 SAATLİK 1 GB İNTERNET KAZANDINIZ!!")
+elif paket==40:
+    print("3 SAATLİK 1 GB İNTERNET KAZANDINIZ!!")
+elif paket==41:
+    print("1 SAATLİK 1 GB İNTERNET KAZANDINIZ!!")
+elif paket==42:
+    print("PAYLAŞ HAFTALIK 1 GB İNTERNET KAZANDINIZ!!")
+elif paket==43:
+    print("PAYLAŞ GÜNLÜK 1 GB İNTERNET KAZANDINIZ!!")
+elif paket==44:
+    print("PAYLAŞ HAFTALIK SAHUR 1 GB İNTERNET KAZANDINIZ!!")
+elif paket==45:
+    print("HAFTALIK HAFTA İÇİ 5 GB İNTERNET KAZANDINIZ!!")
+elif paket==46:
+    print("HAFTALIK HER YİNE 1.000 SMS KAZANDINIZ!!")
+elif paket==47:
+    print("GÜNLÜK HER YÖNE 1.000 SMS")
+elif paket==48:
+    print("GÜNLÜK HER YÖNE 10.000 SMS")
+elif paket==49:
+    print("HAFTA SONU HER YÖNE 1.000 DAKİKA KAZANDINIZ!!")
+elif paket==50:
+    print("GÜNLÜK 1 GB İnstagram KAZANDINIZ!!")
+print( cl("TEKRARDAN SALLAMAK İÇİN UYGULAMAYI KAPATIP TEKRARDAN AÇIN!!","yellow"))
+input()
